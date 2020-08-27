fc82bc0e05a803136313adf9d596857dcf52b381
wbadmin <>
2020-08-27 09:09:38 -0500

    {/src/main/java/com/myspace/drools_training_master/VisaApplication.java}

--- src/main/java/com/myspace/drools_training_master/VisaApplication.java
+++ src/main/java/com/myspace/drools_training_master/VisaApplication.java
@@ -6,12 +6,54 @@ package com.myspace.drools_training_master;
 
 public class VisaApplication implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public VisaApplication() {
-    }
+	private int applicationId;
+	private java.lang.String passportNumber;
+	private java.time.LocalDate visitStartDate;
+	private java.time.LocalDate visitEndDate;
 
+	public VisaApplication() {
+	}
 
+	public int getApplicationId() {
+		return this.applicationId;
+	}
 
+	public void setApplicationId(int applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public java.lang.String getPassportNumber() {
+		return this.passportNumber;
+	}
+
+	public void setPassportNumber(java.lang.String passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+
+	public java.time.LocalDate getVisitStartDate() {
+		return this.visitStartDate;
+	}
+
+	public void setVisitStartDate(java.time.LocalDate visitStartDate) {
+		this.visitStartDate = visitStartDate;
+	}
+
+	public java.time.LocalDate getVisitEndDate() {
+		return this.visitEndDate;
+	}
+
+	public void setVisitEndDate(java.time.LocalDate visitEndDate) {
+		this.visitEndDate = visitEndDate;
+	}
+
+	public VisaApplication(int applicationId, java.lang.String passportNumber,
+			java.time.LocalDate visitStartDate, java.time.LocalDate visitEndDate) {
+		this.applicationId = applicationId;
+		this.passportNumber = passportNumber;
+		this.visitStartDate = visitStartDate;
+		this.visitEndDate = visitEndDate;
+	}
 
 }
