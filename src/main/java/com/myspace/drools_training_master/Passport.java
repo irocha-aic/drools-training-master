43b6cabe0c910bc890cfd1a058a77b2ec5389819
wbadmin <>
2020-08-27 09:06:38 -0500

    {/src/main/java/com/myspace/drools_training_master/Passport.java}

--- src/main/java/com/myspace/drools_training_master/Passport.java
+++ src/main/java/com/myspace/drools_training_master/Passport.java
@@ -6,12 +6,64 @@ package com.myspace.drools_training_master;
 
 public class Passport implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Passport() {
-    }
+	private java.lang.String passportNumber;
+	private java.lang.String name;
+	private java.time.LocalDate expiresOn;
+	private int unusedVisaPages;
+	private int age;
 
+	public Passport() {
+	}
 
+	public java.lang.String getPassportNumber() {
+		return this.passportNumber;
+	}
 
+	public void setPassportNumber(java.lang.String passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.time.LocalDate getExpiresOn() {
+		return this.expiresOn;
+	}
+
+	public void setExpiresOn(java.time.LocalDate expiresOn) {
+		this.expiresOn = expiresOn;
+	}
+
+	public int getUnusedVisaPages() {
+		return this.unusedVisaPages;
+	}
+
+	public void setUnusedVisaPages(int unusedVisaPages) {
+		this.unusedVisaPages = unusedVisaPages;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Passport(java.lang.String passportNumber, java.lang.String name,
+			java.time.LocalDate expiresOn, int unusedVisaPages, int age) {
+		this.passportNumber = passportNumber;
+		this.name = name;
+		this.expiresOn = expiresOn;
+		this.unusedVisaPages = unusedVisaPages;
+		this.age = age;
+	}
 
 }
